6dbc8d3f98123d749a027b91bfa5b7bac4261b23
jsnyd153 <93287904+jsnyd153@users.noreply.github.com>
2022-03-19 09:50:35 -0400

    functionality for setting menu
    open close toggle
    refactor number scoreing
    colors list

--- js/index.js
+++ js/index.js
@@ -1,20 +1,46 @@
+// ============================================================= build function
+$(".team_settings").each(function () {
+  const teamName = $(this).attr("team"); //Home]
+  const sectionName = $(this).attr("class").split(" ").pop(); //lowercase team name [home]
+
+  //build out the controls
+  $(this).append(
+    `<input
+    type="text"
+    value="${teamName}"
+    maxlength="15"
+    for="team${teamName}"
+    name="${sectionName}Name"
+  />`
+  );
+
+  $(this).append(`<input
+    type="number"
+    for="team${teamName}"
+    name="score${teamName}"
+    min="0"
+    max="1000"
+    pattern="[0-9]*"
+  />`);
+
+  $(this).append(`
+  <div class="color_container ${sectionName}" for="teamAway">
+    <div class="color_dot"></div>
+  </div>
+
+  `);
+}); //build function
+
 $(".score").each(function () {
+  const teamName = $(this).attr("team"); //[Home]
+  const sectionName = $(this).attr("class").split(" ").pop(); //lowercase team name [home]
+  const scoreContainer = $(`.score.${sectionName}`);
+  const mc = new Hammer(this);
   let score = 0;
-  // initialize hammer controls
-  var mc = new Hammer(this);
 
-  //select this specific container to refer to later in functions
-  var scoreContainer = $(this);
-  //make jquery object out of id
-  var targetID = $(scoreContainer).attr("id");
-
-  // locate the input that controls this one by matching id to "for" attribute
-  var inputControl = $("#controls").find(`[for='${targetID}']`);
-
-  //set the text disply => run function after scroe counter is updated
   function updateScoreDisplay() {
     $(scoreContainer).find("h1").html(score);
-    $(inputControl).val(score);
+    $(`input[name="score${sectionName}"]`).val(score);
 
     //set prop based to scale text based on number of digits
     if (score > 99) {
@@ -26,99 +52,152 @@ $(".score").each(function () {
     }
   }
 
+  function increaseScore() {
+    score++;
+    updateScoreDisplay();
+  }
+
+  function decreaseScore() {
+    score--;
+    updateScoreDisplay();
+  }
+
   //run update function to set default values
   updateScoreDisplay();
 
-  //Area Swipe Controls
-
-  // pan gesture support all directions.
-  // this will block the vertical scrolling on a touch-device while on the element
+  // swipe controls
   mc.get("swipe").set({ direction: Hammer.DIRECTION_ALL });
 
   mc.on("swipeup", function () {
-    score++;
-    updateScoreDisplay();
+    increaseScore();
   });
 
   mc.on("swipedown", function () {
-    score--;
-    updateScoreDisplay();
+    decreaseScore();
   });
 
   // Button Click Controls
-
   $(this)
     .find(".up")
     .click(function () {
-      score++;
-      updateScoreDisplay();
+      increaseScore();
     });
 
   $(this)
     .find(".down")
     .click(function () {
-      score--;
-      updateScoreDisplay();
+      decreaseScore();
     });
 
-  // Nav Controls
-
+  //reset
   $("#reset").click(function () {
     score = 0;
     $("article").find("h1").html(score);
     updateScoreDisplay();
   });
-
-  //Input controls
-
-  $(inputControl).on("input", function () {
-    let newValue = $(this).val();
-    console.log(newValue);
-
-    if (newValue.length === 0) {
-      console.log("empty");
-      score = 0;
-    } else {
-      //parse to remove leading 0s
-      score = parseInt(newValue, 10);
-    }
-    updateScoreDisplay();
-  });
 });
 
-//menu toggle button
-$("#navToggle").click(function () {
-  $(this).toggleClass("navOpen");
-  $("#controls").toggleClass("navOpen");
-});
-
+// ============================================================= Colors Feature
 $(".color_container").each(function () {
-  const color_container = $(this);
-  let selectedColor;
+  const sectionName = $(this)
+    .parent(".team_settings")
+    .attr("class")
+    .split(" ")
+    .pop(); //lowercase team name [home]
+  const team = $(this).parent(".team_settings").attr("team"); //capitalized team name [Home]
+  const sectionID = "colorChange" + team; //[colorChangeHome]
 
-  // toggle open the list when dot is clicked
   $(this)
     .find(".color_dot")
     .click(function () {
-      $(color_container).toggleClass("isOpen");
+      $(this).parent(".color_container").toggleClass("isOpen");
     });
 
+  //add new color definitions here
+  const colorsOptions = [
+    {
+      colorName: "red",
+    },
+    {
+      colorName: "pink",
+    },
+    {
+      colorName: "gold",
+    },
+    {
+      colorName: "blue",
+    },
+    {
+      colorName: "green",
+    },
+    {
+      colorName: "black",
+    },
+    {
+      colorName: "silver",
+    },
+    {
+      colorName: "orange",
+    },
+  ];
+
+  $(this).append(
+    `<form id="${sectionID}"><ul class="colorPanel color_list"></ul></form>`
+  );
+
+  const container = $(`#${sectionID}`);
+
+  $.each(colorsOptions, function (i, e) {});
+
+  var cList = $(this).find(".color_list");
+  $.each(colorsOptions, function (i, e) {
+    var li = $("<li/>").appendTo(cList);
+    var inputs = $("<input/>")
+      .attr("type", "radio")
+      .attr("id", e.colorName + team)
+      .attr("name", "color" + team)
+      .attr("value", e.colorName)
+      // .text(e.name)
+      .appendTo(li);
+    var labels = $("<label/>")
+      .attr("for", e.colorName + team)
+      .text(e.colorName)
+      .appendTo(li);
+
+    //cList.append(item);
+  });
+
+  //set default cheked values
+  $(".team_settings.home").find('input[value="red"]').prop("checked", true);
+  $(".team_settings.away").find('input[value="blue"]').prop("checked", true);
+
+  let selectedColor = $(this).find('input[type="radio"]:checked').val();
+
   function setNewColorValue() {
-    $(color_container)
+    console.log(selectedColor);
+    $(`.color_container.${sectionName}`)
       .find(".color_dot")
       .removeClass()
       .addClass("color_dot")
       .addClass(selectedColor);
+
+    $(`#team${team}`)
+      .removeClass()
+      .addClass(`score ${sectionName}`)
+      .addClass(selectedColor);
   }
 
-  setNewColorValue();
-
-  const radioButtons = $(this).find("input");
-
-  $("#colorChange").change(function () {
-    newValue = $("input[name='colorHome']:checked").val();
-    selectedColor = newValue;
-
+  $(`#colorChange${team}`).change(function () {
+    selectedColor = $(this).find('input[type="radio"]:checked').val();
+    $(this).parents(".color_container").toggleClass("isOpen");
     setNewColorValue();
   });
+  setNewColorValue();
+});
+
+// ============================================================= menu toggle button
+$("#navToggle").click(function () {
+  $(this).toggleClass("navOpen");
+  $("#controls").toggleClass("navOpen");
+  $("main").toggleClass("navOpen");
 });
